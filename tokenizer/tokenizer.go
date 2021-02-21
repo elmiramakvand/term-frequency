8485cae7023101043a43e82d3ca8d3ebfb8a6805
elmira <64211287+elmiramakvand@users.noreply.github.com>
2021-02-21 07:53:45 +0330

    Update tokenizer.go

--- tokenizer/tokenizer.go
+++ tokenizer/tokenizer.go
@@ -5,31 +5,27 @@ import (
 	"strings"
 )
 
-type queryStrings []string
+type queryString string
 
 // standard tokenizer : This tokenizer splits the text field into tokens,
 // treating whitespace and punctuation as delimiters. Delimiter characters are discarded
-func StandardTokenizer(qs queryStrings) []string {
+func StandardTokenizer(qs queryString) []string {
 	var tokens []string
-	for _, queryString := range qs {
-		step1 := strings.ToLower(queryString)
+	        step1 := strings.ToLower(qs)
 		var re = regexp.MustCompile(`(^\.*)| \.| *\. |@|'|\?|\(|\)|"|“|”|,|-|:|\.*$`)
 		step2 := re.ReplaceAllString(step1, " ")
 		tokens = append(tokens, strings.Fields(step2)...)
-	}
 	return tokens
 }
 
 // keyword tokenizer : This tokenizer treats the entire text field as a single token.
-func KeywordTokenizer(qs queryStrings, tokens []string) []string {
+func KeywordTokenizer(qs queryString, tokens []string) []string {
 	var result []string
-	for _, queryString := range qs {
-		found := Find(tokens, strings.ToLower(queryString))
+	found := Find(tokens, strings.ToLower(qs))
 		if !found {
 			//Value not found in slice
-			result = append(result, strings.ToLower(queryString))
+			result = append(result, strings.ToLower(qs))
 		}
-	}
 	return result
 }
 
